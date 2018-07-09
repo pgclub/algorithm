96cbcea855952fd368f77deb3983c9ddcaa31418
lili <LL@192.168.2.103>
2018-07-09 23:11:06 +0800

    二分法查找

---
+++ src/main/java/com/lili/algorithm/search/BinarySearch.java
@@ -0,0 +1,28 @@
+package com.lili.algorithm.search;
+
+import java.util.Arrays;
+
+/**
+ * @author LL
+ * @QQ:1059997113
+ * @date 2018年7月9日 下午10:52:21
+ * @Description 二分法查找
+ */
+public class BinarySearch {
+	public static int search(int value, int[] arr) {
+		int min = 0;
+		int max = arr.length - 1;
+		Arrays.sort(arr);
+		while (min <= max) {
+			int mid = min + (max - min) / 2;
+			if(value > arr[mid]) {
+				min = mid + 1;
+			} else if (value < arr[mid]) {
+				max = mid - 1;
+			} else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+}
---
+++ src/test/java/com/lili/test/search/Main.java
@@ -0,0 +1,17 @@
+package com.lili.test.search;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import com.lili.algorithm.search.BinarySearch;
+
+public class Main {
+
+	private int[] arr = { 3, 5, 2, 1, 7, 8 };
+
+	@Test
+	public void test() {
+		assertEquals(1, BinarySearch.search(2, arr));
+	}
+}
